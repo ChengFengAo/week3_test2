44870bc04d4fa1f6dc46b03cad94485c0a060cec
ChengFeng AO <414356022@qq.com>
2018-04-05 18:51:36 +0800

    practice05

--- src/main/java/practice05/Student.java
+++ src/main/java/practice05/Student.java
@@ -1,4 +1,16 @@
 package practice05;
 
-public class Student {
+public class Student extends Person {
+    int klass;
+    Student(String name,int age,int klass) {
+        super(name,age);
+        this.klass=klass;
+    }
+    public int getKlass() {
+     return  this.klass=klass;
+    }
+
+    public String introduce() {
+        return super.introduce()+" I am a Student. I am at Class "+ this.getKlass()+".";
+    }
 }
