36db865d5c5948f4a70d393a166abfa776d2987d
ChengFeng AO <414356022@qq.com>
2018-04-05 12:50:45 +0800

    practice02

--- src/main/java/practice02/Student.java
+++ src/main/java/practice02/Student.java
@@ -1,4 +1,27 @@
 package practice02;
 
-public class Student {
+public class Student extends Person {
+    //String name;
+    int kclass;
+    Student(String name, int age, int kclass) {
+        super(name, age);
+        this.kclass = kclass;
+    }
+
+    public String getName() {
+        return super.getName();
+    }
+
+    public int getAge() {
+        return  super.getAge();
+    }
+
+    public int getKlass() {
+        return  this.kclass;
+    }
+
+    public String introduce() {
+        String result = "I am a Student. I am at Class "+this.kclass+".";
+        return result;
+    }
 }
