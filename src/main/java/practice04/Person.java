3fadb0bcd0613bb690697d19474abf3783839a90
ChengFeng AO <414356022@qq.com>
2018-04-05 18:32:39 +0800

    practice04

--- src/main/java/practice04/Person.java
+++ src/main/java/practice04/Person.java
@@ -1,4 +1,24 @@
 package practice04;
 
 public class Person {
+    String name;
+    int age;
+    Person(String name, int age) {
+        this.name=name;
+        this.age=age;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+    public int getAge() {
+        return this.age;
+    }
+    public String basicIntroduce(){
+     return "My name is "+this.getName()+"."+" I am "+this.getAge()+" years old.";
+    }
+
+    public String introduce() {
+       return basicIntroduce();
+    }
 }
--- src/main/java/practice04/Student.java
+++ src/main/java/practice04/Student.java
@@ -1,4 +1,16 @@
 package practice04;
 
-public class Student {
+public class Student extends Person {
+    int klass;
+    Student(String name,int age,int klass) {
+        super(name,age);
+        this.klass=klass;
+    }
+    public int getKlass() {
+        return this.klass;
+    }
+
+    public String introduce() {
+        return super.basicIntroduce()+ " I am a Student. I am at Class "+this.getKlass()+".";
+    }
 }
