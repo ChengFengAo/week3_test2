1048f422cca572f452c17c4df8b63946d5cdeb4b
ChengFeng AO <414356022@qq.com>
2018-04-05 22:14:38 +0800

    practice06

--- src/main/java/practice06/Teacher.java
+++ src/main/java/practice06/Teacher.java
@@ -1,4 +1,31 @@
 package practice06;
 
-public class Teacher {
+public class Teacher extends Person {
+    private int klass;
+
+     Teacher(String name, int age) {
+        super(name, age);
+    }
+
+    Teacher(String name, int age, int klass) {
+        this(name, age);
+        this.klass = klass;
+    }
+
+    public int getKlass() {
+
+        return this.klass;
+    }
+
+    public String introduce() {
+        String result="" ;
+        if (this.getKlass()==0) {
+            result = super.introduce() + " I am a Teacher. I teach No Class.";
+        } else {
+            result=super.introduce()+" I am a Teacher. I teach Class "+this.getKlass()+".";
+        }
+        return result;
+
+    }
+
 }
